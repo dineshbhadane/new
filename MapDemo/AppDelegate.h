a765490d692661e3690610bb4d4fe544c4e6b13e
Minakshi Bhalerao <bhaleraompb@gmail.com>
2017-06-11 12:56:47 +0530

    Initial Commit

---
+++ MapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapDemo
+//
+//  Created by Felix ITs 02 on 11/06/17.
+//  Copyright © 2017 atish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapDemo
+//
+//  Created by Felix ITs 02 on 11/06/17.
+//  Copyright © 2017 atish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
